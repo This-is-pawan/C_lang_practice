babfa73a92f81a61b087d431f30c49b03dc1e2c5
pawan <pawanjalandhara@gmail.com>
2026-02-26 16:52:08 +0530

    all_operators

--- output/operations.c
+++ output/operations.c
@@ -1,3 +1,4 @@
+// Arthmatic operations
 // #include <stdio.h>
 // int main(){
 
@@ -10,13 +11,14 @@
 //  printf("a * b = %d\n",a*b);
 //  printf("a / b = %d\n",a/b);
 //  printf("a modle b = %d\n",a%b);
-// // result
-// //  a + b = 40
-// // a - b = 28
-// // a * b = 204
-// // a / b = 5
-// // a modle b = 4
+ // result
+ //  a + b = 40
+ // a - b = 28
+// a * b = 204
+ // a / b = 5
+ // a modle b = 4
 // }
+
 // #include <stdio.h>
 // int main(){
 
@@ -30,28 +32,47 @@
 //  printf("a * b = %f\n",a*b);
 //  printf("a / b = %f\n",a/b);
  
-// // result
-// // a + b = 40.340000
-// // a - b = 27.660000
-// // a * b = 215.560005
-// // a / b = 5.362776
+ // result
+ // a + b = 40.340000
+ // a - b = 27.660000
+ // a * b = 215.560005
+ // a / b = 5.362776
 // }
+// realtional operations
+// #include <stdio.h>
+// int main(){
 
-#include <stdio.h>
-int main(){
-
- int a,b;
- a = 34;
- b = 34;
+//  int a,b;
+//  a = 34;
+//  b = 34;
  
- printf("a== b = %d\n",a!=b);
- printf("a== b = %d\n",a==b);
- printf("a > b = %d\n",a>b);
- printf("a < b = %d\n",a<b);
+//  printf("a== b = %d\n",a!=b);
+//  printf("a== b = %d\n",a==b);
+//  printf("a > b = %d\n",a>b);
+//  printf("a < b = %d\n",a<b);
  
 // result
 // a== b = 0 false
 // a== b = 1 true
 // a > b = 0 false
 // a < b = 0 false
+// }
+// logical operations
+// #include <stdio.h>
+// int main(){
+
+//  int a,b;
+//  a = 34;
+//  b = 34;
+ 
+//  printf("a && b = %d\n",a&&b==24);
+//  printf("a|| b = %d\n",a||b==34);
+
+ 
+ 
+// result
+// a && b = 0
+// a|| b = 1
+// a ! b = 0
 }
+
