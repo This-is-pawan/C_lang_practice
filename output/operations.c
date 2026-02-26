57f56e6de17510a3d6b279c4621f1c685b0c466f
pawan <pawanjalandhara@gmail.com>
2026-02-26 16:29:00 +0530

    relational operator

--- output/operations.c
+++ output/operations.c
@@ -17,22 +17,41 @@
 // // a / b = 5
 // // a modle b = 4
 // }
+// #include <stdio.h>
+// int main(){
+
+//  int a;
+//  float b;
+//  a = 34;
+//  b = 6.34;
+ 
+//  printf("a + b = %f\n",a+b);
+//  printf("a - b = %f\n",a-b);
+//  printf("a * b = %f\n",a*b);
+//  printf("a / b = %f\n",a/b);
+ 
+// // result
+// // a + b = 40.340000
+// // a - b = 27.660000
+// // a * b = 215.560005
+// // a / b = 5.362776
+// }
+
 #include <stdio.h>
 int main(){
 
- int a;
- float b;
+ int a,b;
  a = 34;
- b = 6.34;
+ b = 34;
  
- printf("a + b = %f\n",a+b);
- printf("a - b = %f\n",a-b);
- printf("a * b = %f\n",a*b);
- printf("a / b = %f\n",a/b);
+ printf("a== b = %d\n",a!=b);
+ printf("a== b = %d\n",a==b);
+ printf("a > b = %d\n",a>b);
+ printf("a < b = %d\n",a<b);
  
 // result
-// a + b = 40.340000
-// a - b = 27.660000
-// a * b = 215.560005
-// a / b = 5.362776
+// a== b = 0 false
+// a== b = 1 true
+// a > b = 0 false
+// a < b = 0 false
 }
