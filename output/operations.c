efc070664c1f4f79f893a5a636b5c17e8b9580c2
pawan <pawanjalandhara@gmail.com>
2026-02-26 16:22:20 +0530

    operations

---
+++ output/operations.c
@@ -0,0 +1,38 @@
+// #include <stdio.h>
+// int main(){
+
+//  int a,b;
+//  a = 34;
+//  b = 6;
+ 
+//  printf("a + b = %d\n",a+b);
+//  printf("a - b = %d\n",a-b);
+//  printf("a * b = %d\n",a*b);
+//  printf("a / b = %d\n",a/b);
+//  printf("a modle b = %d\n",a%b);
+// // result
+// //  a + b = 40
+// // a - b = 28
+// // a * b = 204
+// // a / b = 5
+// // a modle b = 4
+// }
+#include <stdio.h>
+int main(){
+
+ int a;
+ float b;
+ a = 34;
+ b = 6.34;
+ 
+ printf("a + b = %f\n",a+b);
+ printf("a - b = %f\n",a-b);
+ printf("a * b = %f\n",a*b);
+ printf("a / b = %f\n",a/b);
+ 
+// result
+// a + b = 40.340000
+// a - b = 27.660000
+// a * b = 215.560005
+// a / b = 5.362776
+}
