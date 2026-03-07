efce8ae6428b353b7cb0a0e3474f4b9c5d1cc84c
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2026-03-07 08:32:00 +0530

    Update operations.c

--- output/operations.c
+++ output/operations.c
@@ -461,7 +461,28 @@ int main()
 
     return 0;
 }
+int factorial(int number)
+{
+    if (number == 0 || number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return number * factorial(number - 1);
+    }
+}
+// Factorial
+#include <stdio.h>
 
+int main()
+{
+    int num;
+    printf("Enter the number you want the factorial of: ");
+    scanf("%d", &num);
+    printf("The factorial of %d is %d\n", num, factorial(num));
+    return 0;
+}
 
 
 
